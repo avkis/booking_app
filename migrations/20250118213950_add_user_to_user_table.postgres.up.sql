10ba184a4af2e2aa55bf3c7cbaeee64d485b8b52
Aleksandr Kiselev <avkiselev@gmail.com>
2025-01-19 00:44:53 +0300

    Migrations for test user added.

---
+++ migrations/20250118213950_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('John','Doe','admin@bookings.com','$2a$12$MDXUg9Odm5eMegYtw.5FV.Zej6G3cHhoEV2GFqrPZCkdZ38m0pcBi',3,'2025-01-18 00:00:00','2025-01-18 00:00:00');
